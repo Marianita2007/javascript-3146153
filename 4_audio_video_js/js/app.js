06c76eb7fb68338cc5d96d94477ffff6224e9ff7
Marianita2007 <gomezcorreamariana2@gmail.com>
2025-08-14 15:34:41 -0500

    audio parte 1

---
+++ 4_audio_video_js/js/app.js
@@ -0,0 +1,7 @@
+const video = document.querySelector("#video")
+const playVideo = document.querySelector("#play-video")
+const stopvideo = document.querySelector("#stop-video")
+const playStopimg1 = document.querySelector("#play-stop-img1")
+const playStopimg2 = document.querySelector("#play-stop-img2")
+
+console.log(video, playVideo, stopvideo, playStopimg1, playStopimg2)
