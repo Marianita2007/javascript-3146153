3e966fa093bc1360d31efecb717d56ef6ea70de3
Marianita2007 <gomezcorreamariana2@gmail.com>
2025-08-21 15:44:04 -0500

    Add files via upload

--- videojuegohcj/js/app.js
+++ videojuegohcj/js/app.js
@@ -2,6 +2,10 @@
 const caballero = document.querySelectorAll(".caballero-img")
 const cofre = document.querySelectorAll(".cofre")
 const fueguito = document.querySelectorAll(".enemigo-fuego")
+const audio1 = document.querySelector("#audio-1");
+const audio2 = document.querySelector("#audio-2");
+const audio3 = document.querySelector("#audio-3")
+
 
 
 caballero.forEach( function(caballero){
@@ -9,6 +13,14 @@ caballero.forEach( function(caballero){
     caballero.addEventListener( 'click', function () {
         caballero.classList.add( 'saltar' )
         console.log ("click")
+
+        if ( audio1.paused ) {
+        audio1.play()
+        
+        }else{
+        audio1.pause()
+        
+}
        
         
 } )
@@ -20,6 +32,14 @@ cofre.forEach( function(cofre){
     cofre.addEventListener( 'click', function () {
         cofre.classList.add( 'desaparecer' )
         console.log ("click")
+
+        if ( audio2.paused ) {
+        audio2.play()
+        
+        }else{
+        audio2.pause()
+        
+}
        
         
 } )
@@ -32,6 +52,14 @@ fueguito.forEach( function(fueguito){
     fueguito.addEventListener( 'click', function () {
         fueguito.classList.add( 'desaparecer' )
         console.log ("click")
+
+        if ( audio3.paused ) {
+        audio3.play()
+        
+        }else{
+        audio3.pause()
+        
+}
        
         
 } )
